7a445dcb07d4d09a36186842c51bf56f4406d176
godModeD@ <djlevy26@gmail.com>
2022-04-15 14:05:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ const path = require('path');
 
 var app = express();
 
-app.get('/:id', function(req, res) {
+app.get('/:id/:id2', function(req, res) {
     var id = req.params.id;
+    var id2 = req.params.id2
     var date = new Date();
     res.status(200).send(`
     <head>
         <meta property="og:title" content="Minipractice" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://minipractice.net" />
-        <meta property="og:image" content="https://i.ibb.co/${id}/image.png" />
+        <meta property="og:image" content="https://i.ibb.co/${id}/${id2}" />
         <meta name="theme-color" content="#5865F2">
         <meta name="twitter:card" content="summary_large_image">
         <meta property="og:description" content="${date}" />
@@ -26,7 +27,7 @@ app.get('/:id', function(req, res) {
                 margin: 0px;
             }
         </style>
-        <img src="https://i.ibb.co/${id}/image.png" style="display: block;-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;" />
+        <img src="https://i.ibb.co/${id}/${id2}" style="display: block;-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;" />
     </body>
     `)
 })
